44309eb4e2e6e1c784e0d9e1e4a9681c965ed338
김주희 <juhee1687@naver.com>
2023-05-01 18:44:59 +0900

    init DiceGame

---
+++ DiceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceGame
+//
+//  Created by 김주희 on 2023/05/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
